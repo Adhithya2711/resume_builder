37c49c91f227356d2ece6e88c8433ed22fefed44
Adhithya27 <adhithyario8y0@gmail.com>
2023-05-22 19:15:13 +0530

    color.map?

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -8,6 +8,14 @@ function Body() {
             <p className={styles.heading}>Resume Builder</p>
             <div className={styles.toolbar}>
                 <div className={styles.colors}>
+                    {
+                        colors.map(item=>
+                            <span
+                            key={item}
+                            styles={{backgroundColor:item}}
+                            className={styles.color}/>
+                            )
+                    }
                     <span className={styles.color}/>
                     <span className={styles.color}/>
                     <span className={styles.color}/>
