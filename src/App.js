a9c2763e68006ee57226f5622c4182b40847da44
Adhithya27 <adhithyario8y0@gmail.com>
2023-05-22 18:55:11 +0530

    created a body

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Body from './components/Body/Body'
 
 
 
@@ -8,6 +9,8 @@ function App() {
   return (
     <div className="App">
       <Header />
+
+      <Body />
       
     </div>
   );
