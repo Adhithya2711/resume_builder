3b6127894533da195af6a4b6be628047b56f8c12
Adhithya27 <adhithyario8y0@gmail.com>
2023-05-23 17:25:40 +0530

    editor

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 import styles from './Body.module.css';
+import Editor from "../Editor/Editor";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function Body() {
     const colors = ["239ce2","#48bb78","#0bc5ea","#ed8936","#b9d22b","#d22b2b","#1c0793","#454a4d"];
+    
+    const sections = {
+        basicInfo:"Basic Info",
+        workExp: "Work Experience",
+        project:"Projects",
+        eduction:"Eduction",
+        achivement:"Achivements",
+        summary:"Summary",
+        other:"Other",
+    };
     return(
         <div className={styles.container}>
             <p className={styles.heading}>Resume Builder</p>
@@ -19,6 +30,10 @@ function Body() {
                     
                 </div>
                 <button>Download  <FontAwesomeIcon icon={faCircleArrowDown }/>    </button>
+            </div>
+
+            <div className={styles.main}>
+                <Editor sections={sections}/>
             </div>
         </div>
     )
