04d458e8355d8ef1c418ff2c6972dfd6a66e8456
Adhithya27 <adhithyario8y0@gmail.com>
2023-06-09 17:22:20 +0530

    new

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -15,6 +15,8 @@ function Editor(props) {
 
     const [activeInformation, setActiveInformation] = useState(information[sections[Object.keys(sections)[0]]]);
 
+    const [activeDetailIndex, setActiveDetailIndex] = useState(0);
+
     const [sectionTitle, setSectionTitle] = useState(sections[Object.keys(sections)[0]]);
 
     const [values, setValues] = useState({
@@ -262,6 +264,7 @@ function Editor(props) {
                         setValues((prev) => ({ ...prev, github: event.target.value })
                         )
                     } />
+
             </div>
             <div className={styles.row}>
                 <InputControl
@@ -364,116 +367,190 @@ function Editor(props) {
     const handleSubmission = () => {
         switch (sections[activeSectionKey]) {
             case sections.basicInfo: {
-                const tempDetail={
-                    name:values.name,
-                    title:values.title,
-                    linkedin:values.linkedin,
-                    github:values.github,
-                    portfolio:values.portfolio,
-                    email:values.email,
-                    phone:values.phone,
+                const tempDetail = {
+                    name: values.name,
+                    title: values.title,
+                    linkedin: values.linkedin,
+                    github: values.github,
+                    portfolio: values.portfolio,
+                    email: values.email,
+                    phone: values.phone,
 
                 };
+
+                props.setInformation(prev => ({
+                    ...prev, [sections.basicInfo]
+                        : {
+                        ...prev[sections.basicInfo],
+                        detail: tempDetail,
+                        sectionTitle,
+                    },
+                }
+                ));
+                break;
             }
-            {
+            case sections.workExp: {
+                const tempDetail = {
+                    certificateLink: values.certificateLink,
+                    title: values.title,
+                    startDate: values.startDate,
+                    endDate: values.endDate,
+                    companyName: values.companyName,
+                    location: values.location,
+                    points: values.points,
 
-                props.setInformation(prev=>({...prev,[sections.basicInfo]:
-                
-                {
+                };
 
-                }}))
-                
+                const tempDetails = [...information[sections.workExp]?.details];
+                tempDetails[activeDetailIndex] = tempDetail;
 
+                props.setInformation(prev => ({
+                    ...prev, [sections.workExp]
+                        : {
+                        ...prev[sections.workExp],
+                        details: tempDetail,
+                        sectionTitle,
+                    },
+                }));
+                break;
+            }
 
+            case sections.project: {
+                const tempDetail = {
+                    link: values.link,
+                    title: values.title,
+                    overview: values.overview,
+                    github: values.github,
+                    points: values.points,
 
-                break}
-            
+                };
+
+                const tempDetails = [...information[sections.project]?.details];
+                tempDetails[activeDetailIndex] = tempDetail;
+
+                props.setInformation(prev => ({
+                    ...prev, [sections.project]
+                        : {
+                        ...prev[sections.project],
+                        details: tempDetail,
+                        sectionTitle,
+                    },
+                }));
+                break;
+            }
+            case sections.education: {
+                const tempDetail = {
+
+                    title: values.title,
+                    college: values.college,
+                    startDate: values.startDate,
+                    endDate: values.endDate,
+
+                };
+
+                const tempDetails = [...information[sections.education]?.details];
+                tempDetails[activeDetailIndex] = tempDetail;
+
+                props.setInformation(prev => ({
+                    ...prev, [sections.education]
+                        : {
+                        ...prev[sections.education],
+                        details: tempDetail,
+                        sectionTitle,
+                    },
+                }));
+
+                break;
+            }
         }
-    }
+    };
+
+                useEffect(() => {
+
+                    const activeInfo = information[sections[activeSectionKey]];
+
+                    setActiveInformation(activeInfo);
+                    setSectionTitle(sections[activeSectionKey]);
+                    setActiveDetailIndex(0);
+                    setValues({
+                        name: activeInfo?.detail?.name || "",
+                        overview: activeInfo?.details ? activeInfo.details[0]?.overview || "" : "",
+                        link: activeInfo?.details ? activeInfo.details[0]?.link || "" : "",
+                        certificateLink: activeInfo?.details ? activeInfo.details[0]?.certificateLink || "" : "",
+                        companyName: activeInfo?.details ? activeInfo.details[0]?.companyName || "" : "",
+                        location: activeInfo?.details ? activeInfo.details[0]?.location || "" : "",
+                        startDate: activeInfo?.details ? activeInfo.details[0]?.startDate || "" : "",
+                        endDate: activeInfo?.details ? activeInfo.details[0]?.endDate || "" : "",
+                        points: activeInfo?.details ? activeInfo.details[0]?.points ?
+                            [...activeInfo.details[0]?.points] : ""
+                            : activeInfo?.points
+                                ? [...activeInfo.points]
+                                : "",
+
+                        title: activeInfo?.details ? activeInfo.details[0]?.title || ""
+                            : activeInfo?.details?.title || "",
+                        linkedin: activeInfo?.detail?.linkedin || "",
+                        github: activeInfo?.details ? activeInfo.details[0]?.github || ""
+                            : activeInfo?.details?.github || "",
+                        portfolio: activeInfo?.detail?.portfolio || "",
+                        phone: activeInfo?.detail?.phone || "",
+                        email: activeInfo?.detail?.email || "",
+
+                    });
+                }, [activeSectionKey]);
+
+                return (
+                    <div className={styles.container}>
+                        <div className={styles.header}>
+                            {Object.keys(sections)?.map((key) =>
+                                <div className={`${styles.section} ${activeSectionKey === key ?
+                                    styles.active : ""}`}
+                                    key={key}
+                                    onClick={() => setActiveSectionKey(key)}
+                                >
+                                    {sections[key]}
+                                </div>)}
+                        </div>
+                        <div className={styles.body}>
+                            <InputControl label="Title" placeholder="Enter section title"
+
+                                value={sectionTitle}
+                                onChange={(event) => setSectionTitle(event.target.value)}
+
+                            />
+
+                            <div className={styles.chips}>
+                                {
+                                    activeInformation?.details ? activeInformation?.details?.map((item, index) => (
+
+                                        <div className={`${styles.chip}${activeDetailIndex === index ? styles.active : ""
+                                            }`} key={item.title + index}
+                                            onClick={() => setActiveDetailIndex(index)}
+                                        >
+
+                                            <p>
+                                                {sections[activeSectionKey]} {index = 1}
+                                            </p>
+                                            <FontAwesomeIcon icon={faXmark} />
+                                        </div>
+
+                                    )) : ""
+                                }
 
 
-
-
-
-    useEffect(() => {
-
-        const activeInfo = information[sections[activeSectionKey]];
-
-        setActiveInformation(activeInfo);
-        setSectionTitle(sections[activeSectionKey]);
-        setValues({
-            name: activeInfo?.detail?.name || "",
-            overview: activeInfo?.details ? activeInfo.details[0]?.overview || "" : "",
-            link: activeInfo?.details ? activeInfo.details[0]?.link || "" : "",
-            certificateLink: activeInfo?.details ? activeInfo.details[0]?.certificateLink || "" : "",
-            startDate: activeInfo?.details ? activeInfo.details[0]?.startDate || "" : "",
-            endDate: activeInfo?.details ? activeInfo.details[0]?.endDate || "" : "",
-            points: activeInfo?.details ? activeInfo.details[0]?.points ?
-                [...activeInfo.details[0]?.points] : ""
-                : activeInfo?.points
-                    ? [...activeInfo.points]
-                    : "",
-
-            title: activeInfo?.details ? activeInfo.details[0]?.title || ""
-                : activeInfo?.details?.title || "",
-            linkedin: activeInfo?.detail?.linkedin || "",
-            github: activeInfo?.details ? activeInfo.details[0]?.github || ""
-                : activeInfo?.details?.github || "",
-            portfolio:  activeInfo?.detail?.portfolio || "",
-            phone: activeInfo?.detail?.phone || "",
-            email: activeInfo?.detail?.email || "",
-
-        });
-    }, [activeSectionKey]);
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.header}>
-                {Object.keys(sections)?.map((key) =>
-                    <div className={`${styles.section} ${activeSectionKey === key ?
-                        styles.active : ""}`}
-                        key={key}
-                        onClick={() => setActiveSectionKey(key)}
-                    >
-                        {sections[key]}
-                    </div>)}
-            </div>
-            <div className={styles.body}>
-                <InputControl label="Title" placeholder="Enter section title"
-
-                    value={sectionTitle}
-                    onChange={(event) => setSectionTitle(event.target.value)}
-
-                />
-
-                <div className={styles.chips}>
-                    {
-                        activeInformation?.details ? activeInformation?.details?.map((item, index) => (
-
-                            <div className={styles.chip} key={item.title + index}>
-
-                                <p>
-                                    {sections[activeSectionKey]} {index = 1}
-                                </p>
-                                <FontAwesomeIcon icon={faXmark} />
                             </div>
 
-                        )) : ""
-                    }
+
+                            {generateBody()}
 
 
-                </div>
+                            <button onClick={handleSubmission}>Save</button>
 
 
-                {generateBody()}
+                        </div>
+                    </div>
+                );
+        }
+    
 
-
-                <button onClick={handleSubmission}>Save</button>
-
-
-            </div>
-        </div>
-    )
-}
-
-export default Editor;
+        export default Editor;
